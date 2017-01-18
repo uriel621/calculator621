f099218111c283ddc4d4d8cfdc5ece89be4cc868
Ever Garcia <uriel621@live.com>
2017-01-17 21:00:54 -0800

    funciones de operacion

--- vanilla/script.js
+++ vanilla/script.js
@@ -19,7 +19,7 @@ var addOperatortoArray = function(inputOperator){
         numberArray[1] = "";
     }
     numberIndex = 1;
-    document.queryselector("#input").value += inputOperator;
+    document.querySelector("#input").value += inputOperator;
     operator = inputOperator;
 }
 
@@ -27,4 +27,66 @@ var resetCalculator = function(){
     document.querySelector("#display").value = "";
     numberArray = ["", ""];
     number_index = 0;
+}
+
+/*
+Operations functions
+*/
+
+var addition = function(num1, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if(isNaN(num1) || isNaN(num2)){
+        alert("It is not going to work like that");
+        return false;
+    }
+    else {
+        return (num1 + num2);
+    }
+}
+
+var subtraction = function(num1, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if(isNaN(num1) || isNaN(num2)){
+        alert("It is not going to work like that");
+        return false;
+    }
+    else {
+        return (num1 - num2)
+    }
+}
+
+var multiplication = function(num1, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if(isNaN(num1) || isNaN(num2)){
+        alert("It is not going to work like that");
+        return false;
+    }
+    else {
+        num1 * num2;
+    }
+}
+
+var division = function(num1, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if(isNaN(num1) || isNaN(num2)){
+        alert("It is not going to work like that");
+        return false;  
+    }
+    else {
+        if(num2 > 0){
+            return (num1 / num2)
+        }
+        else {
+            alert("You can not divide by zero")
+            return false
+        }
+    }
 }
